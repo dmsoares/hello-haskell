6b84ebb527a7817c216f0fce48ecdaa8e43428c5
Decio Soares <decio.msoares@gmail.com>
2021-09-18 15:17:55 +0100

    changes to print3broken

--- print3broken.hs
+++ print3broken.hs
@@ -1,5 +1,17 @@
 -- | Fix the following module
 
+-- module Print3Broken where
+
+-- printSecond :: IO ()
+-- printSecond = do
+--   putStrLn greeting
+--
+-- main :: IO ()
+-- main = do
+--   putStrLn greeting
+--     printSecond
+--     where greeting = "Yarrrrr"
+
 module Print3Broken where
 
 printSecond :: String -> IO ()
