379c3e8254bd52a2c0ceebd771e1fec9298d1ad7
Decio Soares <decio.msoares@gmail.com>
2021-11-07 00:38:20 +0000

    Add Ch12 first two exercises.

---
+++ ch12-exercises.hs
@@ -0,0 +1,66 @@
+-- |
+
+module Ch12Exercises where
+
+-- Determine the kinds
+-- 1. The kind is *
+-- 2. The kind of a is * and the kind of f is * -> *
+
+-- String processing
+-- 1.
+import Data.Maybe (isNothing)
+notThe :: String -> Maybe String
+notThe word =
+  if word == "the"
+  then Nothing
+  else Just word
+
+replaceThe :: String -> String
+replaceThe = unwords . map replace . words
+  where
+    replace w = case notThe w of
+      Nothing -> "a"
+      Just word -> word
+
+-- 2.
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel = count . words
+  where
+    count (w0:rest@(w1:ws)) =
+      if isNothing (notThe w0) && head w1 `elem` "aeiou"
+      then (+1) $ count rest
+      else count rest
+    count _ = 0
+
+-- 3.
+vowels = "aeiou"
+
+isVowel :: Char -> Bool
+isVowel = flip elem vowels
+
+getVowels :: String -> String
+getVowels = filter isVowel
+
+countVowels :: String -> Integer
+countVowels = toInteger . length . getVowels
+
+-- Validate the word
+newtype Word' =
+  Word' String
+  deriving (Eq, Show)
+
+isWord :: String -> Bool
+isWord candidate = fst t <= snd t
+  where
+    t = foldr (\a (numVowels, numConsonants)
+               -> if isVowel a
+                  then (numVowels+1, numConsonants)
+                  else (numVowels, numConsonants+1))
+        (0, 0) candidate
+
+mkWord :: String -> Maybe Word'
+mkWord candidate = if isWord candidate
+                   then Just (Word' candidate)
+                   else Nothing
+
+-- It's only Natural
