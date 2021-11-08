523ffa034112db13970946447a325b36076d0eca
Decio Soares <decio.msoares@gmail.com>
2021-11-08 20:51:18 +0000

    Add Ch12's "It's only Natural" exercise.

--- ch12-exercises.hs
+++ ch12-exercises.hs
@@ -64,3 +64,20 @@ mkWord candidate = if isWord candidate
                    else Nothing
 
 -- It's only Natural
+data Nat =
+    Zero
+  | Succ Nat
+  deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero = 0
+natToInteger (Succ n) = 1 + natToInteger n
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat i
+  | i < 0 = Nothing
+  | i == 0 = Just Zero
+  | otherwise = Just $ go i
+  where
+    go 0 = Zero
+    go i = Succ (go $ i - 1)
