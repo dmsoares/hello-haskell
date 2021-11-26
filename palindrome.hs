15a7992c364c2837930501b78949b22fdd3150fb
Decio Soares <decio.msoares@gmail.com>
2021-11-26 23:32:02 +0000

    Change palindrome to work with sentences.

--- palindrome.hs
+++ palindrome.hs
@@ -2,12 +2,20 @@
 
 module Palindrome where
 
+import Data.Char
 import Control.Monad
 import System.Exit (exitSuccess)
+
+strToLower :: String -> String
+strToLower = map toLower
+
+onlyLetters :: String -> String
+onlyLetters = filter $ flip elem $ ['A'..'z'] ++ ['0'..'9']
 
 palindrome :: IO ()
 palindrome = forever $ do
   line1 <- getLine
-  case (line1 == reverse line1) of
-    True -> putStrLn "It's a palindrome!"
-    False -> exitSuccess
+  if (strToLower . onlyLetters $ line1) ==
+    (reverse . strToLower . onlyLetters $ line1)
+    then putStrLn "It's a palindrome!"
+    else exitSuccess
