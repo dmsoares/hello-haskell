af1b6e7a455e21ef3cacf06c6080db4ad58648dd
Decio Soares <decio.msoares@gmail.com>
2022-01-23 14:25:19 +0000

    Add Ch15 Monoid Exercises #3.

--- ch15-monoid-exercises.hs
+++ ch15-monoid-exercises.hs
@@ -11,6 +11,9 @@ main = do
   quickCheck (semigroupAssoc :: IdentAssoc String)
   quickCheck (monoidLeftIdentity :: Identity String -> Bool)
   quickCheck (monoidRightIdentity :: Identity String -> Bool)
+  quickCheck (semigroupAssoc :: TwoAssoc String String)
+  quickCheck (monoidLeftIdentity :: Two String String -> Bool)
+  quickCheck (monoidRightIdentity :: Two String String -> Bool)
 
 semigroupAssoc ::
   (Eq m, Semigroup m) =>
@@ -41,6 +44,7 @@ instance Semigroup Trivial where
 
 instance Monoid Trivial where
   mempty = Trivial
+  mappend = (<>)
 
 instance Arbitrary Trivial where
   arbitrary = return Trivial
@@ -58,9 +62,26 @@ instance Semigroup a => Semigroup (Identity a) where
 
 instance Monoid a => Monoid (Identity a) where
   mempty = Identity mempty
+  mappend = (<>)
 
 instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = arbitrary >>= return . Identity
 
 type IdentAssoc a =
   Identity a -> Identity a -> Identity a -> Bool
+
+-- 3.
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+  mempty = Two mempty mempty
+  mappend = (<>)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = arbitrary >>= \a -> arbitrary >>= (return . Two a)
+
+type TwoAssoc a b =
+  Two a b -> Two a b -> Two a b -> Bool
