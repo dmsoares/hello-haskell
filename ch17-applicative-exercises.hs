efd0a8a5052ce04e575a361cb9d6ea67ed6a56ce
Decio Soares <decio.msoares@gmail.com>
2022-02-17 00:16:38 +0000

    Add Ch17-Applicative Lookup exercise 3.

--- ch17-applicative-exercises.hs
+++ ch17-applicative-exercises.hs
@@ -1,5 +1,7 @@
 module Ch17ApplicativeExercises where
 
+import           Data.List (elemIndex)
+
 -- Lookups
 -- 1.
 added :: Maybe Integer
@@ -15,3 +17,16 @@ z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
 
 tupled :: Maybe (Integer, Integer)
 tupled = (,) <$> y <*> z
+
+-- 3.
+x :: Maybe Int
+x = elemIndex 3 [1, 2, 3, 4, 5]
+
+w :: Maybe Int
+w = elemIndex 4 [1, 2, 3, 4, 5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = max' <$> x <*> w
