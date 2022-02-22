ce345a823bcb265eebd7551ea8261a252f669cd1
Decio Soares <decio.msoares@gmail.com>
2022-02-22 01:40:50 +0000

    Update Ch17 ZipList Applicative Exercise.

--- ch17-applicative-exercises.hs
+++ ch17-applicative-exercises.hs
@@ -111,11 +111,14 @@ instance Applicative List where
       append Nil list         = list
       append (Cons x xs) list = Cons x (append xs list)
 
-instance Arbitrary a => Arbitrary (List a) where
-  arbitrary = do
-    a <- arbitrary
-    b <- arbitrary
-    return $ Cons a (Cons b Nil)
+instance Arbitrary a => Arbitrary (List a) where -- from stackoverflow
+  arbitrary = sized go
+    where
+      go 0 = pure Nil
+      go n = do
+        xs <- go (n - 1)
+        x <- arbitrary
+        return (Cons x xs)
 
 instance Eq a => EqProp (List a) where
   (=-=) = eq
@@ -135,3 +138,44 @@ concat' = fold append Nil
 flatMap :: (a -> List b) -> List a -> List b
 flatMap f Nil         = Nil
 flatMap f (Cons a as) = append (f a) (flatMap f as)
+
+-- ZipList Applicative Exercise
+take' :: Int -> List a -> List a
+take' 0 _           = Nil
+take' _ Nil         = Nil
+take' n (Cons a as) = Cons a (take' (n - 1) as)
+
+newtype ZipList' a
+  = ZipList' (List a)
+  deriving (Eq, Show)
+
+instance Eq a => EqProp (ZipList' a) where
+  xs =-= ys = xs' `eq` ys'
+    where
+      xs' =
+        let (ZipList' l) = xs
+         in take' 3000 l
+      ys' =
+        let (ZipList' l) = ys
+         in take' 3000 l
+
+instance Functor ZipList' where
+  fmap f (ZipList' xs) =
+    ZipList' $ fmap f xs
+
+instance Applicative ZipList' where
+  pure x = ZipList' $ go x
+    where
+      go x = Cons x (go x)
+  (<*>) (ZipList' Nil) _ = ZipList' Nil
+  (<*>) _ (ZipList' Nil) = ZipList' Nil
+  (<*>) (ZipList' fList) (ZipList' xList) =
+    ZipList' $ go fList xList
+    where
+      go Nil _ = Nil
+      go _ Nil = Nil
+      go (Cons f fs) (Cons x xs) =
+        Cons (f x) (go fs xs)
+
+instance Arbitrary a => Arbitrary (ZipList' a) where
+  arbitrary = ZipList' <$> arbitrary
