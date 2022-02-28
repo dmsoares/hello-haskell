989c1d1aeef1ff2a589ee5dd2550c2b7f712eb08
Decio Soares <decio.msoares@gmail.com>
2022-02-28 06:04:15 +0000

    Add Ch18 bind in terms of fmap and join.

---
+++ ch18-monad-exercises.hs
@@ -0,0 +1,7 @@
+module Ch18MonadExercises where
+
+import           Control.Monad
+
+-- Create bind in terms of fmap and join
+bind :: Monad m => (a -> m b) -> m a -> m b
+bind f m = join $ fmap f m
