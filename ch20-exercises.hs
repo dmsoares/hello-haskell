c25a9a59060f8b3b4c8b994a97569887e496e25d
Decio <decio.soares@academiadecodigo.org>
2022-04-15 16:24:56 +0100

    add Ch20Foldable exercises 4-8

--- ch20-exercises.hs
+++ ch20-exercises.hs
@@ -15,3 +15,29 @@ product = getProduct . foldMap Product
 -- 3.
 elem :: (Foldable t, Eq a) => a -> t a -> Bool
 elem a = getAny . foldMap (Any . (== a))
+
+-- 4.
+minimum :: (Foldable t, Ord a) => t a -> Maybe a
+minimum = foldr f Nothing
+  where
+    f a Nothing = Just a
+    f a (Just a') = Just $ min a a'
+
+-- 5.
+maximum :: (Foldable t, Ord a) => t a -> Maybe a
+maximum = foldr f Nothing
+  where
+    f a Nothing = Just a
+    f a (Just a') = Just $ max a a'
+
+-- 6.
+null :: Foldable t => t a -> Bool
+null = foldr (const $ const False) True
+
+-- 7.
+length :: Foldable t => t a -> Int
+length = foldr (const (+ 1)) 0
+
+-- 8.
+toList :: Foldable t => t a -> [a]
+toList = foldr (:) []
