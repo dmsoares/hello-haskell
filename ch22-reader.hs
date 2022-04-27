3be3fd78b96dbaeea8ce8a26cb80ba4584c9de1a
Decio <decio.soares@academiadecodigo.org>
2022-04-27 22:47:53 +0100

    add Ch22Reader notes

---
+++ ch22-reader.hs
@@ -0,0 +1,70 @@
+module Ch22Reader where
+
+import Control.Applicative
+
+boop = (* 2)
+
+doop = (+ 10)
+
+bip :: Integer -> Integer
+bip = boop . doop
+
+bloop :: Integer -> Integer
+bloop = fmap boop doop
+
+bbop :: Integer -> Integer
+bbop = (+) <$> boop <*> doop
+
+duwop :: Integer -> Integer
+duwop = liftA2 (+) boop doop
+
+-- Demonstrating the function Applicative
+newtype HumanName
+  = HumanName String
+  deriving (Eq, Show)
+
+newtype DogName
+  = DogName String
+  deriving (Eq, Show)
+
+newtype Address
+  = Address String
+  deriving (Eq, Show)
+
+data Person = Person
+  { humanName :: HumanName,
+    dogName :: DogName,
+    address :: Address
+  }
+  deriving (Eq, Show)
+
+data Dog = Dog
+  { dogsName :: DogName,
+    dogsAddress :: Address
+  }
+  deriving (Eq, Show)
+
+-- sample data:
+pers :: Person
+pers =
+  Person
+    (HumanName "Big Bird")
+    (DogName "Barkley")
+    (Address "Sesame Street")
+
+chris :: Person
+chris =
+  Person
+    (HumanName "Chris Allen")
+    (DogName "Papu")
+    (Address "Austin")
+
+-- without Reader
+getDog :: Person -> Dog
+getDog p =
+  Dog (dogName p) (address p)
+
+-- with Reader
+getDogR :: Person -> Dog
+getDogR =
+  Dog <$> dogName <*> address
