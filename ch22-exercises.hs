39ba4a74b563706d0afe7ac43fbe660f7aeb8738
Decio <decio.soares@academiadecodigo.org>
2022-04-29 12:41:51 +0100

    add Reader Monad exercise

--- ch22-exercises.hs
+++ ch22-exercises.hs
@@ -68,3 +68,64 @@ instance Applicative (Reader r) where
     Reader r b
   (Reader rab) <*> (Reader ra) =
     Reader $ \r -> rab r $ ra r
+
+-- Reader Monad
+-- 1.
+instance Monad (Reader r) where
+  return = pure
+
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  (Reader ra) >>= aRb = Reader $ \r -> runReader (aRb (ra r)) r
+
+-- 2. Rewrite getDogRM
+newtype HumanName
+  = HumanName String
+  deriving (Eq, Show)
+
+newtype DogName
+  = DogName String
+  deriving (Eq, Show)
+
+newtype Address
+  = Address String
+  deriving (Eq, Show)
+
+data Person = Person
+  { humanName :: HumanName,
+    dogName :: DogName,
+    address :: Address
+  }
+  deriving (Eq, Show)
+
+data Dog = Dog
+  { dogsName :: DogName,
+    dogsAddress :: Address
+  }
+  deriving (Eq, Show)
+
+-- sample data:
+pers :: Person
+pers =
+  Person
+    (HumanName "Big Bird")
+    (DogName "Barkley")
+    (Address "Sesame Street")
+
+chris :: Person
+chris =
+  Person
+    (HumanName "Chris Allen")
+    (DogName "Papu")
+    (Address "Austin")
+
+getDogRM :: Reader Person Dog
+getDogRM = do
+  name <- Reader dogName
+  addy <- Reader address
+  return $ Dog name addy
+
+getDogRM' :: Reader Person Dog
+getDogRM' =
+  Reader dogName >>= \name ->
+    Reader address >>= \addy ->
+      return $ Dog name addy
